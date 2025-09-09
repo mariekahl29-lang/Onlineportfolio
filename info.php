05c54c4bf5fc8228c354355ce07cec999a31dd1b
mariekahl29-lang <marie.kahl29@gmail.com>
2025-09-09 19:34:33 +0200

    Add files via upload

---
+++ info.php
@@ -0,0 +1,4 @@
+<?php
+$title = "INSTAX Mini 12,Magische Augenblicke. Sofort zum Anfassen";
+$description = "Die INSTAX mini 12 Sofortbildkamera nimmt dank des perfekten Selfiespiegels, eines Nahaufnahme-Modus, automatischer Belichtung und eines verstellbaren Suchers Erinnerungen an die schönsten Momente in nur 5 Sekunden fest.";
+?>
